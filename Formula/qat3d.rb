553241169b418317e6a2cf5d885e65ba940b533b
Joseph Boudreau <boudreau@pitt.edu>
2018-08-21 11:40:03 -0400

    depend_on modified--build clause dropped

--- Formula/qat3d.rb
+++ Formula/qat3d.rb
@@ -3,8 +3,8 @@ class Qat3d < Formula
   homepage "qat.pitt.edu"
   url "https://www.qat.pitt.edu/qat-2.0.0.tar.gz"
   sha256 "9541bed6cfe0e546afcf47dc4fe178a36424871f353a20af6272b1bb117d14f4"
-  depends_on 'qt5' => :build 
-  depends_on 'eigen' => :build
+  depends_on 'qt5'
+  depends_on 'eigen' 
   depends_on 'gsl' => :build
   depends_on 'hdf5' => :build
   depends_on 'pkg-config' => :build
