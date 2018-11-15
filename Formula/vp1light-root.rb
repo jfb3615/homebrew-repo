f2f5403e0178f1854708ad484ed61d1ac5f280c1
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-11-14 19:19:23 -0500

    Add files via upload

---
+++ Formula/vp1light-root.rb
@@ -0,0 +1,153 @@
+class Vp1lightRoot < Formula
+  desc "Object oriented framework for large scale data analysis"
+  homepage "https://root.cern.ch/"
+  url "https://root.cern.ch/download/root_v6.14.04.source.tar.gz"
+  version "6.14.04"
+  sha256 "463ec20692332a422cfb5f38c78bedab1c40ab4d81be18e99b50cf9f53f596cf"
+  revision 2
+  head "https://github.com/root-project/root.git"
+
+  bottle do
+    sha256 "ca72af676d7c91ce31dcc96d475f3fc5d7653b8412f6e02bdc5e1df58d85ff24" => :mojave
+    sha256 "3ae6464e7850068f4b86bad5abaa521bb5f681c3eb7218bcc3adc43796a72618" => :high_sierra
+    sha256 "d3ef8ca60ceeaf453006b137740274979548b1443ff0d1383450ece038194330" => :sierra
+  end
+
+  # https://github.com/Homebrew/homebrew-core/issues/30726
+  # strings libCling.so | grep Xcode:
+  #  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
+  #  /Applications/Xcode.app/Contents/Developer
+  pour_bottle? do
+    reason "The bottle hardcodes locations inside Xcode.app"
+    satisfy do
+      MacOS::Xcode.installed? &&
+        MacOS::Xcode.prefix.to_s.include?("/Applications/Xcode.app/")
+    end
+  end
+
+  depends_on "cmake" => :build
+  depends_on "davix"
+  depends_on "fftw"
+  depends_on "gcc" # for gfortran
+  depends_on "graphviz"
+  depends_on "gsl"
+  # Temporarily depend on Homebrew libxml2 to work around a brew issue:
+  # https://github.com/Homebrew/brew/issues/5068
+  depends_on "libxml2" if MacOS.version >= :mojave
+  depends_on "lz4"
+  depends_on "openssl"
+  depends_on "pcre"
+  depends_on "tbb"
+  depends_on "xrootd"
+  depends_on "xz" # for LZMA
+  depends_on "python" => :recommended
+  depends_on "python@2" => :optional
+
+  skip_clean "bin"
+
+  needs :cxx14
+
+  def install
+    # Work around "error: no member named 'signbit' in the global namespace"
+    ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
+
+    # Freetype/afterimage/gl2ps/lz4 are vendored in the tarball, so are fine.
+    # However, this is still permitting the build process to make remote
+    # connections. As a hack, since upstream support it, we inreplace
+    # this file to "encourage" the connection over HTTPS rather than HTTP.
+    inreplace "cmake/modules/SearchInstalledSoftware.cmake",
+              "http://lcgpackages",
+              "https://lcgpackages"
+
+    args = std_cmake_args + %W[
+      -Dgnuinstall=ON
+      -DCMAKE_INSTALL_ELISPDIR=#{elisp}
+      -DCLING_CXX_PATH=clang++
+      -Dbuiltin_freetype=ON
+      -Dbuiltin_cfitsio=OFF
+      -Ddavix=ON
+      -Dfitsio=OFF
+      -Dfftw3=ON
+      -Dfortran=ON
+      -Dgdml=ON
+      -Dmathmore=ON
+      -Dminuit2=ON
+      -Dmysql=OFF
+      -Dpgsql=OFF
+      -Droofit=ON
+      -Dssl=ON
+      -Dimt=ON
+      -Dxrootd=ON
+      -Dtmva=ON
+    ]
+
+    if build.with?("python") && build.with?("python@2")
+      odie "Root: Does not support building both python 2 and 3 wrappers"
+    elsif build.with?("python") || build.with?("python@2")
+      if build.with? "python@2"
+        ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
+        python_executable = Utils.popen_read("which python").strip
+        python_version = Language::Python.major_minor_version("python")
+      elsif build.with? "python"
+        python_executable = Utils.popen_read("which python3").strip
+        python_version = Language::Python.major_minor_version("python3")
+      end
+
+      python_prefix = Utils.popen_read("#{python_executable} -c 'import sys;print(sys.prefix)'").chomp
+      python_include = Utils.popen_read("#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+      args << "-Dpython=ON"
+
+      # cmake picks up the system's python dylib, even if we have a brewed one
+      if File.exist? "#{python_prefix}/Python"
+        python_library = "#{python_prefix}/Python"
+      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
+        python_library = "#{python_prefix}/lib/lib#{python_version}.a"
+      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.dylib"
+        python_library = "#{python_prefix}/lib/lib#{python_version}.dylib"
+      else
+        odie "No libpythonX.Y.{a,dylib} file found!"
+      end
+      args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
+      args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+      args << "-DPYTHON_LIBRARY='#{python_library}'"
+    else
+      args << "-Dpython=OFF"
+    end
+
+    mkdir "builddir" do
+      system "cmake", "..", *args
+
+      # Work around superenv stripping out isysroot leading to errors with
+      # libsystem_symptoms.dylib (only available on >= 10.12) and
+      # libsystem_darwin.dylib (only available on >= 10.13)
+      if MacOS.version < :high_sierra
+        system "xcrun", "make", "install"
+      else
+        system "make", "install"
+      end
+
+      chmod 0755, Dir[bin/"*.*sh"]
+    end
+  end
+
+
+  test do
+    (testpath/"test.C").write <<~EOS
+      #include <iostream>
+      void test() {
+        std::cout << "Hello, world!" << std::endl;
+      }
+    EOS
+    (testpath/"test.bash").write <<~EOS
+      . #{bin}/thisroot.sh
+      root -l -b -n -q test.C
+    EOS
+    assert_equal "\nProcessing test.C...\nHello, world!\n",
+                 shell_output("/bin/bash test.bash")
+
+    if build.with? "python"
+      ENV["PYTHONPATH"] = lib/"root"
+      system "python3", "-c", "import ROOT"
+    end
+  end
+end
