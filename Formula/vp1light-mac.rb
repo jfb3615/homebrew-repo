632fb6e54071a36d4f1de36ab4c54b6d5827f690
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-11-17 08:39:30 -0500

    Update vp1light-mac.rb

--- Formula/vp1light-mac.rb
+++ Formula/vp1light-mac.rb
@@ -11,7 +11,7 @@ class Vp1lightMac < Formula
   depends_on "jfb3615/repo/mcutils"
   depends_on "jfb3615/repo/heppdt"
   def install
-    
+    ENV["ROOTSYS"] = "/usr/local"
     mkdir "builddir" do
        system "cmake", "../Projects/NewVP1Light", *std_cmake_args
        system "make", "install" 
