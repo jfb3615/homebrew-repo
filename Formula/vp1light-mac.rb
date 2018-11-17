5022e0125c2906072d7137752dc3c58b011ed688
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-11-17 08:06:02 -0500

    Update vp1light-mac.rb

--- Formula/vp1light-mac.rb
+++ Formula/vp1light-mac.rb
@@ -11,7 +11,7 @@ class Vp1lightMac < Formula
   depends_on "jfb3615/repo/mcutils"
   depends_on "jfb3615/repo/heppdt"
   def install
-    system "cmake", "../Projects/NewVP1Light", *std_cmake_args
+    system "cmake", "Projects/NewVP1Light", *std_cmake_args
     system "make", "install" # if this fails, try separate make/make install steps
   end
 
