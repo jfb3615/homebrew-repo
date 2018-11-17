7f15886bfedefa256de4f13587c5343d1f2d4144
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-11-17 18:28:43 -0500

    Update vp1light-mac.rb

--- Formula/vp1light-mac.rb
+++ Formula/vp1light-mac.rb
@@ -1,8 +1,8 @@
 class Vp1lightMac < Formula
   desc ""
   homepage "http://atlas-vp1.web.cern.ch/atlas-vp1/home/"
-  url "qat.pitt.edu/other-src/vp1light-1.1.tar.gz"
-  sha256 "14e9f294fc3e3f08fde780ad02cd70cc0f633655505782a7ed63605df547d6e7"
+  url "qat.pitt.edu/other-src/vp1light-1.2.tar.gz"
+  sha256 "0d20e59a348d180f7ed8e86ae43d259b84c954bbce3046183851f8cf7fb8e9bc"
   depends_on "cmake" => :build
   depends_on "jfb3615/repo/vp1light-root"
   depends_on "jfb3615/repo/qat3d"
