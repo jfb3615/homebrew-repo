a7fb12d569cbb9ce965f1ba1b2d9137e2419769b
Joseph Boudreau <atlas@boudreau3949.phyast.pitt.edu>
2018-12-30 14:26:10 -0500

    Fix error in new formula

--- Formula/soqt5.rb
+++ Formula/soqt5.rb
@@ -4,7 +4,7 @@ class Soqt5 < Formula
   url "http://qat.pitt.edu/soqt5-1.0.tar.gz"
   sha256 "2cdd7a87bd6353f9f6da8541d0d1d78711ae030380b96a03ee4344d4f4bf3bce"
   depends_on "qt5"
-  depends_on "jfb3615/coin"
+  depends_on "jfb3615/repo/coin"
 
   def install
     ENV.deparallelize  
