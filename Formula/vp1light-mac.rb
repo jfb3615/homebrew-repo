a6c230577fc13021c80c8a1d0182fbf03e05c746
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-12-30 16:28:59 -0500

    Update vp1light-mac.rb

--- Formula/vp1light-mac.rb
+++ Formula/vp1light-mac.rb
@@ -5,7 +5,9 @@ class Vp1lightMac < Formula
   sha256 "a619c9eaccc8a4b378ba3dcdc766c10efc38435b666465e68ee74f6b130e1091"
   depends_on "cmake" => :build
   depends_on "jfb3615/repo/vp1light-root"
-  depends_on "jfb3615/repo/qat3d"
+  depends_on "jfb3615/repo/coin"
+  depends_on "jfb3615/repo/simage"
+  depends_on "jfb3615/repo/soqt5"
   depends_on "jfb3615/repo/vp1light-googletest"
   depends_on "jfb3615/repo/heputils"
   depends_on "jfb3615/repo/mcutils"
