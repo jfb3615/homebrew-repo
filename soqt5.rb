d9ef9c598ee6f13e5d2ede0b10b71931bd9189ec
Joseph Boudreau <atlas@boudreau3949.phyast.pitt.edu>
2018-12-30 14:19:10 -0500

    Added a formulae for soqt

---
+++ soqt5.rb
@@ -0,0 +1,14 @@
+class Soqt5 < Formula
+  desc "The SoQt package updated from the orginal by Joe Boudreau"
+  homepage "qat.pitt.edu"
+  url "http://qat.pitt.edu/soqt5-1.0.tar.gz"
+  sha256 "2cdd7a87bd6353f9f6da8541d0d1d78711ae030380b96a03ee4344d4f4bf3bce"
+  depends_on "qt5"
+  depends_on "jfb3615/coin"
+
+  def install
+    ENV.deparallelize  
+    system "qmake PREFIX=#{prefix} LIBS=-L/usr/local/lib QMAKE_STRIP="
+    system "make", "install"
+  end
+end
