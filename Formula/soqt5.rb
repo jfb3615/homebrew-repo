1954b411f7e3dcbb9c7dfc89b221cb11e38ddf7b
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-12-30 14:30:17 -0500

    Set directory in build of soqt5

--- Formula/soqt5.rb
+++ Formula/soqt5.rb
@@ -8,6 +8,7 @@ class Soqt5 < Formula
 
   def install
     ENV.deparallelize  
+    system "cd src/Inventor/Qt"
     system "pwd"
     system "ls"
     system "qmake PREFIX=#{prefix} LIBS=-L/usr/local/lib QMAKE_STRIP="
