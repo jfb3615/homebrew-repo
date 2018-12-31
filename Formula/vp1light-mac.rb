85ce52db1202d2179a59af04797f45867e7dd57d
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-12-30 20:50:02 -0500

    Update vp1light-mac.rb

--- Formula/vp1light-mac.rb
+++ Formula/vp1light-mac.rb
@@ -14,12 +14,9 @@ class Vp1lightMac < Formula
   depends_on "jfb3615/repo/heppdt"
   depends_on "clhep"
   def install
-       ENV["ROOTSYS"] = "/usr/local"
-       ENV["LIBPATH"] = "/usr/local/lib/root"
-       ENV["SHLIB_PATH"] = "/usr/local/lib/root"
        mkdir "builddir" do
-       system "source /usr/local/bin/thisroot.sh && cmake", "../Projects/NewVP1Light", *std_cmake_args 
-       system "source /usr/local/bin/thisroot.sh && make", "install" 
+       system "cmake", "../Projects/NewVP1Light", *std_cmake_args 
+       system "make", "install" 
     end 
   end
  
