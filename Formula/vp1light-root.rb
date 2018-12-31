43e183a569f0b2a5ca47f500663eca785d126ae7
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-12-31 12:21:38 -0500

    Update vp1light-root.rb

--- Formula/vp1light-root.rb
+++ Formula/vp1light-root.rb
@@ -1,74 +1,139 @@
-class Vp1lightRoot < Formula
+class VP1LightRoot < Formula
   desc "Object oriented framework for large scale data analysis"
-  homepage "https://root.cern.ch/"
+  homepage "https://root.cern.ch"
   url "https://root.cern.ch/download/root_v6.12.06.source.tar.gz"
   version "6.12.06"
   sha256 "aedcfd2257806e425b9f61b483e25ba600eb0ea606e21262eafaa9dc745aa794"
-  revision 2
-  head "https://github.com/root-project/root.git"
-
-
+  revision 1
+  head "http://root.cern.ch/git/root.git"
 
 
   depends_on "cmake" => :build
-  depends_on "davix"
   depends_on "fftw"
-  depends_on "gcc" # for gfortran
+  depends_on "gcc" # for gfortran.
   depends_on "graphviz"
   depends_on "gsl"
-  # Temporarily depend on Homebrew libxml2 to work around a brew issue:
-  # https://github.com/Homebrew/brew/issues/5068
-  depends_on "libxml2" if MacOS.version >= :mojave
-  depends_on "lz4"
   depends_on "openssl"
   depends_on "pcre"
-  depends_on "tbb"
   depends_on "xrootd"
-  depends_on "xz" # for LZMA
+  depends_on "xz" # For LZMA.
   depends_on "python" => :recommended
   depends_on "python@2" => :optional
-  conflicts_with "lz4", :because => "lz4 is compiled automatically from source in this installation"
-  conflicts_with "root", :because => "vp1light-root compiled w c++14 replaces default root"
-  skip_clean "bin"
 
   needs :cxx14
 
+  skip_clean "bin"
+
   def install
     # Work around "error: no member named 'signbit' in the global namespace"
-    #ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
+    ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
 
-    
-    #  -Dgnuinstall=ON
-    #  -DCMAKE_INSTALL_ELISPDIR=#{elisp}
-    #  -DCLING_CXX_PATH=clang++
-    #  -Dbuiltin_freetype=ON
-    #  -Dbuiltin_cfitsio=OFF
-    #  -Ddavix=ON
-    #  -Dfitsio=OFF
-    #  -Dfftw3=ON
-    #  -Dfortran=ON
-    #  -Dgdml=ON
-    # -Dmathmore=ON
-    #  -Dminuit2=ON
-    #  -Dmysql=OFF
-    #  -Dpgsql=OFF
-    #  -Droofit=ON
-    #  -Dssl=ON
-    #  -Dimt=ON
-    #  -Dxrootd=ON
-    #  -Dtmva=ON
-    #  -Dcxx14=ON
-    ENV["ROOTSYS"]    = "/usr/local"
-    ENV["LIBPATH"]    = "./lib:/usr/local/lib"
-    ENV["SHLIB_PATH"] = "./lib:/usr/local/lib"
-    system "./configure --disable-astiff --disable-bonjour --disable-builtin_afterimage --disable-builtin_unuran --disable-builtin_llvm --disable-explicitlink --disable-gviz  --disable-memstat --disable-pgsql --disable-pythia8 --disable-python --disable-shadowpw  --disable-sqlite --disable-ssl --disable-tmva  --enable-cxx14 --enable-gsl-shared --enable-roofit --prefix=/usr/local"
-    system "make"
-    system "make", "install" 
+    # Freetype/afterimage/gl2ps/lz4 are vendored in the tarball, so are fine.
+    # However, this is still permitting the build process to make remote
+    # connections. As a hack, since upstream support it, we inreplace
+    # this file to "encourage" the connection over HTTPS rather than HTTP.
+    inreplace "cmake/modules/SearchInstalledSoftware.cmake",
+              "http://lcgpackages",
+              "https://lcgpackages"
+
+    args = std_cmake_args + %W[
+      -Dgnuinstall=ON
+      -DCMAKE_INSTALL_ELISPDIR=#{elisp}
+      -Dbuiltin_freetype=ON
+      -Dfftw3=ON
+      -Dfortran=ON
+      -Dgdml=ON
+      -Dmathmore=ON
+      -Dminuit2=ON
+      -Dmysql=OFF
+      -Droofit=ON
+      -Dssl=ON
+      -Dxrootd=ON
+      -DC++14=ON
+    ]
+
+    if build.with?("python") && build.with?("python@2")
+      odie "Root: Does not support building both python 2 and 3 wrappers"
+    elsif build.with?("python") || build.with?("python@2")
+      if build.with? "python@2"
+        ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
+        python_executable = Utils.popen_read("which python").strip
+        python_version = Language::Python.major_minor_version("python")
+      elsif build.with? "python"
+        python_executable = Utils.popen_read("which python3").strip
+        python_version = Language::Python.major_minor_version("python3")
+      end
+
+      python_prefix = Utils.popen_read("#{python_executable} -c 'import sys;print(sys.prefix)'").chomp
+      python_include = Utils.popen_read("#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+      args << "-Dpython=ON"
+
+      # cmake picks up the system's python dylib, even if we have a brewed one
+      if File.exist? "#{python_prefix}/Python"
+        python_library = "#{python_prefix}/Python"
+      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
+        python_library = "#{python_prefix}/lib/lib#{python_version}.a"
+      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.dylib"
+        python_library = "#{python_prefix}/lib/lib#{python_version}.dylib"
+      else
+        odie "No libpythonX.Y.{a,dylib} file found!"
+      end
+      args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
+      args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+      args << "-DPYTHON_LIBRARY='#{python_library}'"
+    else
+      args << "-Dpython=OFF"
+    end
+
+    mkdir "builddir" do
+      system "cmake", "..", *args
+
+      # Work around superenv stripping out isysroot leading to errors with
+      # libsystem_symptoms.dylib (only available on >= 10.12) and
+      # libsystem_darwin.dylib (only available on >= 10.13)
+      if MacOS.version < :high_sierra
+        system "xcrun", "make", "install"
+      else
+        system "make", "install"
+      end
+
+      chmod 0755, Dir[bin/"*.*sh"]
+    end
   end
-  
- 
 
+  def caveats; <<~EOS
+    Because ROOT depends on several installation-dependent
+    environment variables to function properly, you should
+    add the following commands to your shell initialization
+    script (.bashrc/.profile/etc.), or call them directly
+    before using ROOT.
 
+    For bash users:
+      . #{HOMEBREW_PREFIX}/bin/thisroot.sh
+    For zsh users:
+      pushd #{HOMEBREW_PREFIX} >/dev/null; . bin/thisroot.sh; popd >/dev/null
+    For csh/tcsh users:
+      source #{HOMEBREW_PREFIX}/bin/thisroot.csh
+    EOS
+  end
 
+  test do
+    (testpath/"test.C").write <<~EOS
+      #include <iostream>
+      void test() {
+        std::cout << "Hello, world!" << std::endl;
+      }
+    EOS
+    (testpath/"test.bash").write <<~EOS
+      . #{bin}/thisroot.sh
+      root -l -b -n -q test.C
+    EOS
+    assert_equal "\nProcessing test.C...\nHello, world!\n",
+                 shell_output("/bin/bash test.bash")
 
+    if build.with? "python"
+      ENV["PYTHONPATH"] = lib/"root"
+      system "python3", "-c", "import ROOT"
+    end
+  end
 end
