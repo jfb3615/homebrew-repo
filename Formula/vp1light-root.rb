0e5c06cc56443966724d039b73616a6c0bac1a67
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-12-31 10:31:30 -0500

    Update vp1light-root.rb

--- Formula/vp1light-root.rb
+++ Formula/vp1light-root.rb
@@ -58,7 +58,7 @@ class Vp1lightRoot < Formula
     #  -Dtmva=ON
     #  -Dcxx14=ON
     
-    system "./configure --disable-astiff --disable-bonjour --disable-builtin_afterimage --disable-builtin_unuran --disable-builtin_lz4 --disable-builtin_llvm --disable-explicitlink --disable-gviz  --disable-memstat --disable-opengl --disable-pgsql --disable-pythia8 --disable-python --disable-shadowpw  --disable-sqlite --disable-ssl --disable-tmva  --enable-cxx14 --enable-gsl-shared --enable-roofit --prefix=/usr/local"
+    system "./configure --disable-astiff --disable-bonjour --disable-builtin_afterimage --disable-builtin_unuran --disable-builtin_lz4 --disable-builtin_llvm --disable-explicitlink --disable-gviz  --disable-memstat --disable-pgsql --disable-pythia8 --disable-python --disable-shadowpw  --disable-sqlite --disable-ssl --disable-tmva  --enable-cxx14 --enable-gsl-shared --enable-roofit --prefix=/usr/local"
     system "make"
     system "make", "install" 
   end
