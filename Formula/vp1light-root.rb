17854e4cfb095ab05423886b99645bc1375c1c8f
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2018-12-31 10:18:27 -0500

    Update vp1light-root.rb

--- Formula/vp1light-root.rb
+++ Formula/vp1light-root.rb
@@ -1,32 +1,16 @@
 class Vp1lightRoot < Formula
   desc "Object oriented framework for large scale data analysis"
   homepage "https://root.cern.ch/"
-  url "https://root.cern.ch/download/root_v6.14.04.source.tar.gz"
-  version "6.14.04"
-  sha256 "463ec20692332a422cfb5f38c78bedab1c40ab4d81be18e99b50cf9f53f596cf"
+  url "https://root.cern.ch/download/root_v6.12.06.source.tar.gz"
+  version "6.12.06"
+  sha256 "a0ecc8d775d8d812dacbcd9ccf08f1162bd7f0ea3a34a449ce3f86cd3a52b75a"
   revision 2
   head "https://github.com/root-project/root.git"
 
 
-  bottle do
-    root_url "https://qat.pitt.edu/Bottles"
-    rebuild 1
-    sha256 "f23e48fc10e0e8bb73c861ef7200ef284eda4e82718ff38b893a87d95eb5db36" => :high_sierra
-  end
 
-  # https://github.com/Homebrew/homebrew-core/issues/30726
-  # strings libCling.so | grep Xcode:
-  #  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
-  #  /Applications/Xcode.app/Contents/Developer
-  #pour_bottle? do
-  #  reason "The bottle hardcodes locations inside Xcode.app"
-  #  satisfy do
-  #    MacOS::Xcode.installed? &&
-  #      MacOS::Xcode.prefix.to_s.include?("/Applications/Xcode.app/")
-  #  end
-  #end
 
-  depends_on "cmake" => :build
+#  depends_on "cmake" => :build
   depends_on "davix"
   depends_on "fftw"
   depends_on "gcc" # for gfortran
@@ -50,109 +34,38 @@ class Vp1lightRoot < Formula
 
   def install
     # Work around "error: no member named 'signbit' in the global namespace"
-    ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
+    #ENV.delete("SDKROOT") if DevelopmentTools.clang_build_version >= 900
 
-    # Freetype/afterimage/gl2ps/lz4 are vendored in the tarball, so are fine.
-    # However, this is still permitting the build process to make remote
-    # connections. As a hack, since upstream support it, we inreplace
-    # this file to "encourage" the connection over HTTPS rather than HTTP.
-    inreplace "cmake/modules/SearchInstalledSoftware.cmake",
-              "http://lcgpackages",
-              "https://lcgpackages"
-
-    args = std_cmake_args + %W[
-      -Dgnuinstall=ON
-      -DCMAKE_INSTALL_ELISPDIR=#{elisp}
-      -DCLING_CXX_PATH=clang++
-      -Dbuiltin_freetype=ON
-      -Dbuiltin_cfitsio=OFF
-      -Ddavix=ON
-      -Dfitsio=OFF
-      -Dfftw3=ON
-      -Dfortran=ON
-      -Dgdml=ON
-      -Dmathmore=ON
-      -Dminuit2=ON
-      -Dmysql=OFF
-      -Dpgsql=OFF
-      -Droofit=ON
-      -Dssl=ON
-      -Dimt=ON
-      -Dxrootd=ON
-      -Dtmva=ON
-      -Dcxx14=ON
-    ]
-
-    if build.with?("python") && build.with?("python@2")
-      odie "Root: Does not support building both python 2 and 3 wrappers"
-    elsif build.with?("python") || build.with?("python@2")
-      if build.with? "python@2"
-        ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
-        python_executable = Utils.popen_read("which python").strip
-        python_version = Language::Python.major_minor_version("python")
-      elsif build.with? "python"
-        python_executable = Utils.popen_read("which python3").strip
-        python_version = Language::Python.major_minor_version("python3")
-      end
-
-      python_prefix = Utils.popen_read("#{python_executable} -c 'import sys;print(sys.prefix)'").chomp
-      python_include = Utils.popen_read("#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
-      args << "-Dpython=ON"
-
-      # cmake picks up the system's python dylib, even if we have a brewed one
-      if File.exist? "#{python_prefix}/Python"
-        python_library = "#{python_prefix}/Python"
-      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
-        python_library = "#{python_prefix}/lib/lib#{python_version}.a"
-      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.dylib"
-        python_library = "#{python_prefix}/lib/lib#{python_version}.dylib"
-      else
-        odie "No libpythonX.Y.{a,dylib} file found!"
-      end
-      args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
-      args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
-      args << "-DPYTHON_LIBRARY='#{python_library}'"
-    else
-      args << "-Dpython=OFF"
-    end
-
-    mkdir "builddir" do
-      system "cmake", "..", *args
-
-      # Work around superenv stripping out isysroot leading to errors with
-      # libsystem_symptoms.dylib (only available on >= 10.12) and
-      # libsystem_darwin.dylib (only available on >= 10.13)
-      if MacOS.version < :high_sierra
-        system "xcrun", "make", "install"
-      else
-        system "make", "install"
-      end
-
-      chmod 0755, Dir[bin/"*.*sh"]
-    end
+    
+    #  -Dgnuinstall=ON
+    #  -DCMAKE_INSTALL_ELISPDIR=#{elisp}
+    #  -DCLING_CXX_PATH=clang++
+    #  -Dbuiltin_freetype=ON
+    #  -Dbuiltin_cfitsio=OFF
+    #  -Ddavix=ON
+    #  -Dfitsio=OFF
+    #  -Dfftw3=ON
+    #  -Dfortran=ON
+    #  -Dgdml=ON
+    # -Dmathmore=ON
+    #  -Dminuit2=ON
+    #  -Dmysql=OFF
+    #  -Dpgsql=OFF
+    #  -Droofit=ON
+    #  -Dssl=ON
+    #  -Dimt=ON
+    #  -Dxrootd=ON
+    #  -Dtmva=ON
+    #  -Dcxx14=ON
+    
+    system("./configure --disable-astiff --disable-bonjour --disable-builtin_afterimage --disable-builtin_unuran --disable-builtin_lz4 --disable-builtin_llvm --disable-explicitlink --disable-gviz  --disable-memstat --disable-opengl --disable-pgsql --disable-pythia8 --disable-python --disable-shadowpw  --disable-sqlite --disable-ssl --disable-tmva  --enable-cxx14 --enable-gsl-shared --enable-roofit --prefix=/usr/local")
+    system("make")
+    system("make","install")
   end
   
  
 
 
 
-  test do
-    (testpath/"test.C").write <<~EOS
-      #include <iostream>
-      void test() {
-        std::cout << "Hello, world!" << std::endl;
-      }
-    EOS
-    (testpath/"test.bash").write <<~EOS
-      . #{bin}/thisroot.sh
-      root -l -b -n -q test.C
-    EOS
-    assert_equal "\nProcessing test.C...\nHello, world!\n",
-                 shell_output("/bin/bash test.bash")
 
-    if build.with? "python"
-      ENV["PYTHONPATH"] = lib/"root"
-      system "python3", "-c", "import ROOT"
-    end
-  end
 end
