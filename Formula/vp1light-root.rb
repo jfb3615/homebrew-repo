4be9cf48d168d1a15a6f7bc0336ed6314214fb83
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2019-01-01 12:33:46 -0500

    Update vp1light-root.rb

--- Formula/vp1light-root.rb
+++ Formula/vp1light-root.rb
@@ -1,26 +1,22 @@
 class Vp1lightRoot < Formula
   desc "Object oriented framework for large scale data analysis"
   homepage "https://root.cern.ch/"
-  url "https://root.cern.ch/download/root_v6.14.04.source.tar.gz"
-  version "6.14.04"
-  sha256 "463ec20692332a422cfb5f38c78bedab1c40ab4d81be18e99b50cf9f53f596cf"
-  revision 2
+  url "https://root.cern.ch/download/root_v6.14.06.source.tar.gz"
+  version "6.14.06"
+  sha256 "0fb943b61396f282b289e35c455a9ab60126229be1bd3f04a8f00b37c13ab432"
   head "https://github.com/root-project/root.git"
-
-
- 
 
   # https://github.com/Homebrew/homebrew-core/issues/30726
   # strings libCling.so | grep Xcode:
   #  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
   #  /Applications/Xcode.app/Contents/Developer
-  #pour_bottle? do
-  #  reason "The bottle hardcodes locations inside Xcode.app"
-  #  satisfy do
-  #    MacOS::Xcode.installed? &&
-  #      MacOS::Xcode.prefix.to_s.include?("/Applications/Xcode.app/")
-  #  end
-  #end
+  pour_bottle? do
+    reason "The bottle hardcodes locations inside Xcode.app"
+    satisfy do
+      MacOS::Xcode.installed? &&
+        MacOS::Xcode.prefix.to_s.include?("/Applications/Xcode.app/")
+    end
+  end
 
   depends_on "cmake" => :build
   depends_on "davix"
@@ -34,11 +30,10 @@ class Vp1lightRoot < Formula
   depends_on "lz4"
   depends_on "openssl"
   depends_on "pcre"
+  depends_on "python"
   depends_on "tbb"
   depends_on "xrootd"
   depends_on "xz" # for LZMA
-  depends_on "python" => :recommended
-  depends_on "python@2" => :optional
 
   skip_clean "bin"
 
@@ -56,61 +51,36 @@ class Vp1lightRoot < Formula
               "http://lcgpackages",
               "https://lcgpackages"
 
+    py_exe = Utils.popen_read("which python3").strip
+    py_prefix = Utils.popen_read("python3 -c 'import sys;print(sys.prefix)'").chomp
+    py_inc = Utils.popen_read("python3 -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+
     args = std_cmake_args + %W[
-      -Dgnuinstall=ON
-      -DCMAKE_INSTALL_ELISPDIR=#{elisp}
       -DCLING_CXX_PATH=clang++
-      -Dbuiltin_freetype=ON
+      -DCMAKE_INSTALL_ELISPDIR=#{elisp}
+      -DPYTHON_EXECUTABLE=#{py_exe}
+      -DPYTHON_INCLUDE_DIR=#{py_inc}
+      -DPYTHON_LIBRARY=#{py_prefix}/Python
       -Dbuiltin_cfitsio=OFF
+      -Dbuiltin_freetype=ON
       -Ddavix=ON
-      -Dfitsio=OFF
       -Dfftw3=ON
+      -Dfitsio=OFF
       -Dfortran=ON
       -Dgdml=ON
+      -Dgnuinstall=ON
+      -Dimt=ON
       -Dmathmore=ON
       -Dminuit2=ON
       -Dmysql=OFF
       -Dpgsql=OFF
+      -Dpython=ON
       -Droofit=ON
       -Dssl=ON
-      -Dimt=ON
-      -Dxrootd=ON
       -Dtmva=ON
+      -Dxrootd=ON
       -Dcxx14=ON
     ]
-
-    if build.with?("python") && build.with?("python@2")
-      odie "Root: Does not support building both python 2 and 3 wrappers"
-    elsif build.with?("python") || build.with?("python@2")
-      if build.with? "python@2"
-        ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
-        python_executable = Utils.popen_read("which python").strip
-        python_version = Language::Python.major_minor_version("python")
-      elsif build.with? "python"
-        python_executable = Utils.popen_read("which python3").strip
-        python_version = Language::Python.major_minor_version("python3")
-      end
-
-      python_prefix = Utils.popen_read("#{python_executable} -c 'import sys;print(sys.prefix)'").chomp
-      python_include = Utils.popen_read("#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
-      args << "-Dpython=ON"
-
-      # cmake picks up the system's python dylib, even if we have a brewed one
-      if File.exist? "#{python_prefix}/Python"
-        python_library = "#{python_prefix}/Python"
-      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
-        python_library = "#{python_prefix}/lib/lib#{python_version}.a"
-      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.dylib"
-        python_library = "#{python_prefix}/lib/lib#{python_version}.dylib"
-      else
-        odie "No libpythonX.Y.{a,dylib} file found!"
-      end
-      args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
-      args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
-      args << "-DPYTHON_LIBRARY='#{python_library}'"
-    else
-      args << "-Dpython=OFF"
-    end
 
     mkdir "builddir" do
       system "cmake", "..", *args
@@ -127,10 +97,22 @@ class Vp1lightRoot < Formula
       chmod 0755, Dir[bin/"*.*sh"]
     end
   end
-  
- 
 
+  def caveats; <<~EOS
+    Because ROOT depends on several installation-dependent
+    environment variables to function properly, you should
+    add the following commands to your shell initialization
+    script (.bashrc/.profile/etc.), or call them directly
+    before using ROOT.
 
+    For bash users:
+      . #{HOMEBREW_PREFIX}/bin/thisroot.sh
+    For zsh users:
+      pushd #{HOMEBREW_PREFIX} >/dev/null; . bin/thisroot.sh; popd >/dev/null
+    For csh/tcsh users:
+      source #{HOMEBREW_PREFIX}/bin/thisroot.csh
+  EOS
+  end
 
   test do
     (testpath/"test.C").write <<~EOS
@@ -146,10 +128,8 @@ class Vp1lightRoot < Formula
     assert_equal "\nProcessing test.C...\nHello, world!\n",
                  shell_output("/bin/bash test.bash")
 
-    if build.with? "python"
-      ENV["PYTHONPATH"] = lib/"root"
-      system "python3", "-c", "import ROOT"
-    end
+    # Test Python module
+    ENV["PYTHONPATH"] = lib/"root"
+    system "python3", "-c", "import ROOT"
   end
 end
-
