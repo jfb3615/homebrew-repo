e38c92450d7bf0afac6baec24bb286302e06ec1a
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2019-01-01 21:07:02 -0500

    Update vp1light-googletest.rb

--- Formula/vp1light-googletest.rb
+++ Formula/vp1light-googletest.rb
@@ -11,4 +11,9 @@ class Vp1lightGoogletest < Formula
     system "cmake", "CMakeLists.txt", *std_cmake_args
     system "make", "install" 
   end
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3d414b2ea3fd25c6c2f16746d39906251a99bd0fc8e93d481feda90dfe766799" => :mojave
+  end
+
 end
