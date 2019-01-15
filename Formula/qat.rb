419e4de344487d16f226adb6a9a9f592c14e6880
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2019-01-15 10:10:37 -0500

    Update qat.rb

--- Formula/qat.rb
+++ Formula/qat.rb
@@ -27,7 +27,7 @@ class Qat < Formula
   bottle do
     root_url "https://qat.pitt.edu/Bottles"
     cellar :any
-    rebuild 1
+    rebuild 2
     sha256 "3618ec5fb42a91bba92dc60bf424b3568049dcb31c35372319dacf6ea4e6103d" => :high_sierra
     rebuild 1
     sha256 "c3f694a9f71f916b7874e0f65c4b2b12a10465605f25e5062d40bc25f3c84380" => :mojave
