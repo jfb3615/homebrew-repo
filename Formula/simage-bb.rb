8356bb458699f7e72767435357b7acb5c1573030
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2020-05-05 18:44:05 -0400

    Update simage-bb.rb

--- Formula/simage-bb.rb
+++ Formula/simage-bb.rb
@@ -1,7 +1,7 @@
 class SimageBb < Formula
   desc "The simage library for texture mapping with Coin"
   homepage "https://coin3d.github.io"
-  url "simage-master-5May2020-00.tar.gz"
+  url "qat.pitt.edu/simage-master-5May2020-00.tar.gz"
   sha256 "57694751ffb5d0f1e0f5fde6a84129c3c7cdec3378be2122f6ee39566a03c8ba"
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
