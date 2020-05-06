dccd61a1ef5e1f219f2e178511ccfe8293655a4c
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2020-05-06 07:28:58 -0400

    Update coin-bb.rb

--- Formula/coin-bb.rb
+++ Formula/coin-bb.rb
@@ -1,7 +1,7 @@
 class CoinBb < Formula
   desc "The Coin implemention of the Open Inventor Toolkit"
   homepage "https://coin3d.github.io"
-  url "https://qat.pitt.edu/coin-6May2020.tar.gz"
+  url "https://qat.pitt.edu/coin-6May2020-00.tar.gz"
   sha256 "e5b430a000d549cf6e1e7a10b2254e2042b5f75a3ba03451febeefc29f69acff"
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
