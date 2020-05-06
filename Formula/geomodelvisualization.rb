b3145bec1e4b714f28c91748047567375e29d8c8
Joseph F. Boudreau <boudreau@pitt.edu>
2020-05-06 15:02:19 -0400

    geomodelvisualization added

---
+++ Formula/geomodelvisualization.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+class Geomodelvisualization < Formula
+  desc "Visualizatin for the GeoModel Toolkit"
+  homepage "https://gitlab.cern.ch/GeoModelDev/geomodelvisualization"
+
+  # SOURCES
+  head do
+    url "https://gitlab.cern.ch/GeoModelDev/geomodelvisualization.git"
+    version "master"
+  end
+
+  # DEPENDENCIES
+  depends_on "cmake" => :build
+  depends_on "qt"
+  depends_on "eigen"
+  depends_on "simage-bb"
+  depends_on "coin-bb"
+  depends_on "soqt-bb"
+  depends_on "geomodelcore"
+  depends_on "geomodelio"
+
+  # INSTALLATION INSTRUCTIONS
+  def install
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+   end
+  end
+
+end
