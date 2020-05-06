d254f435a3f9793fbcbdc79b63f17f85f65d8d6b
Joseph F. Boudreau <boudreau@pitt.edu>
2020-05-06 15:24:21 -0400

    geomodeltools added

---
+++ Formula/geomodeltools.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+class Geomodeltools < Formula
+  desc "Handy tools in the GeoModel tool suite"
+  homepage "https://gitlab.cern.ch/GeoModelDev/GeoModelTools"
+
+  # SOURCES
+  head do
+    url "https://gitlab.cern.ch/GeoModelDev/GeoModelIO.git"
+  end
+
+  # DEPENDENCIES
+  depends_on "cmake" => :build
+  depends_on "eigen"
+  depends_on "nlohmann-json"
+  depends_on "geomodelcore"
+  depends_on "geomodelio"
+  
+  # INSTALLATION INSTRUCTIONS
+  def install
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+   end
+  end
+
+end
