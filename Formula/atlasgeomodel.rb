b45a6cad7212bb05507530e8507c4bb6695bd348
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2020-05-06 16:40:03 -0400

    Update atlasgeomodel.rb

--- Formula/atlasgeomodel.rb
+++ Formula/atlasgeomodel.rb
@@ -3,12 +3,13 @@
 class Atlasgeomodel < Formula
   desc "Meta-package including gmex and some sample inputs for ATLAS"
   homepage "https://gitlab.cern.ch/GeoModelATLAS/geometry-data"
-  url "https://qat.pitt.edu/atlasgeomodel.tar.gz"
+  url "https://qat.pitt.edu/atlasgeomodel-1.0.tar.gz"
 
   # DEPENDENCIES
   depends_on "cmake" => :build
   depends_on "geomodelvisualization"
   depends_on "geomodeltools"
   depends_on "atlasgeometryfiles"
+  depends_on "handsonplugins"
 
 end
