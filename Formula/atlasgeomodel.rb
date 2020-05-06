17e99e39cd65f41fb4b2b5a755836130c4cdcc07
Joseph F. Boudreau <boudreau@pitt.edu>
2020-05-06 16:12:06 -0400

    meta-package atlasgeomodel added

---
+++ Formula/atlasgeomodel.rb
@@ -0,0 +1,13 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+class Atlasgeomodel < Formula
+  desc "Meta-package including gmex and some sample inputs for ATLAS"
+  homepage "https://gitlab.cern.ch/GeoModelATLAS/geometry-data"
+
+  # DEPENDENCIES
+  depends_on "cmake" => :build
+  depends_on "geomodelvisualization"
+  depends_on "geomodeltools"
+  depends_on "atlasgeometryfiles"
+
+end
