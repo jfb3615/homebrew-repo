783addffb74d3cae3b0cb9bc0d3c247a97a12a84
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2020-05-06 07:26:14 -0400

    Update coin-bb.rb

--- Formula/coin-bb.rb
+++ Formula/coin-bb.rb
@@ -1,8 +1,8 @@
 class CoinBb < Formula
   desc "The Coin implemention of the Open Inventor Toolkit"
   homepage "https://bitbucket.org/Coin3D"
-  url "https://qat.pitt.edu/coin-bb-1.0.tar.gz"
-  sha256 "e153260c99101beebc0acf33536f31501dfaf1e065bf3b0d8f69a7cc4a5d4747"
+  url "https://qat.pitt.edu/coin-6May2020.tar.gz"
+  sha256 "e5b430a000d549cf6e1e7a10b2254e2042b5f75a3ba03451febeefc29f69acff"
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   def install
@@ -10,15 +10,6 @@ class CoinBb < Formula
       system "cmake", "..", "-DCMAKE_CXX_FLAGS=-std=c++14", *std_cmake_args
       system "make",      "install" 
     end
-  end
-
-  bottle do
-    root_url "https://qat.pitt.edu/Bottles"
-    cellar :any
-    sha256 "b418e0593be09c6a9bcbe1ddd924f318870f1fd6f2303f32481ef2d322ce9087" => :high_sierra
-    rebuild 1
-    sha256 "4fcfebd03208cfe804bf3b2ae2a7a41c729d6f9af82f786d8b4fb687c561e1bc" => :mojave
-
   end
 
 end
