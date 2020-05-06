272ce4ebe6aa167ee7ad9f4b09bfa9e30feec78f
Joseph F. Boudreau <boudreau@pitt.edu>
2020-05-06 15:58:57 -0400

    atlasgeometryfiles added

---
+++ Formula/atlasgeometryfiles.rb
@@ -0,0 +1,23 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+class Atlasgeometryfiles < Formula
+  desc "Some very simple plugins to exercise the plugins methods"
+  homepage "https://gitlab.cern.ch/GeoModelATLAS/geometry-data"
+
+  # SOURCES
+  head do
+    url "https://gitlab.cern.ch/GeoModelATLAS/geometry-data.git"
+  end
+
+  # DEPENDENCIES
+  depends_on "cmake" => :build
+  depends_on "db-browser-for-sqlite"
+  
+  # INSTALLATION INSTRUCTIONS
+  def install
+    system "mkdir -p #{prefix}/share/ATLASGeoModel/"
+    system "cp geometry  #{prefix}/share/ATLASGeoModel"
+    system "cp materials #{prefix}/share/ATLASGeoModel"
+  end
+
+end
