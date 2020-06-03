afe5f3234909c70f11dd1fa0224c5ed098846f86
Joseph F. Boudreau <boudreau@pitt.edu>
2020-06-03 08:14:17 -0400

    Package g4 first attempt

--- Formula/geant4.rb
+++ Formula/geant4.rb
@@ -5,6 +5,7 @@ class Geant4 < Formula
   sha256 "4fd64149ae26952672a81ce5579d3806fda4bd251d486897093ac57633a42b7e"
   depends_on "cmake" => :build
   depends_on "doxygen" => "build"
+  depends_on "xerces-c"
    def install
      mkdir "builddir" do
      system "cmake", "..",   "-DGEANT4_USE_GDML=ON -DGEANT4_BUILD_MULTITHREADED=ON", *std_cmake_args
