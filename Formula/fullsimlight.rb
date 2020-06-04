20c771d5af698f8733fcbc2f4d788818babfbfcc
Joseph F. Boudreau <boudreau@pitt.edu>
2020-06-03 21:43:47 -0400

    new package fullsimlight

---
+++ Formula/fullsimlight.rb
@@ -0,0 +1,25 @@
+class Fullsimlight < Formula
+
+  desc "FullSimLight"
+  homepage "https://gitlab.cern.ch/GeoModelDev/FullSimLight"
+
+  head do
+    url "https://gitlab.cern.ch/GeoModelDev/FullSimLight.git"
+  end
+
+  # DEPENDENCIES
+  depends_on "cmake" => :build
+  depends_on "eigen"
+  depends_on "geomodelg4"
+  depends_on "geant4"
+  depends_on "nlohmann_json"
+
+  # INSTALLATION INSTRUCTIONS
+  def install
+    mkdir "build" do
+      system "cmake", "-G", "Unix Makefiles", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+   end
+  end
+end
