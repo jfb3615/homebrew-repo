a954c1497ff7a44c8f3eabbb61a4243c6da9d33f
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2020-06-05 14:47:35 -0400

    Update spacetime.rb

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -6,7 +6,6 @@ class Spacetime < Formula
   homepage "qat.pitt.edu"
   url "https://www.qat.pitt.edu/spacetime-3.0.0.tar.gz"
   sha256 "e37a5ede0e0b706cc35baef7bd0b02c2345a5d1e190cb15cf6c063f3c1ae3b60"
-  depends_on 'qt5' 
   depends_on 'eigen' 
   depends_on 'pkg-config' 
   # This line caused a complaint.  No longer needed?  :-->.   needs :cxx11 
