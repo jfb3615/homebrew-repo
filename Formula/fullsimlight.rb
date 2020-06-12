b76e46f943152ce4b4d4790adb527c0f0aa95389
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2020-06-12 12:32:17 -0400

    Update fullsimlight.rb

--- Formula/fullsimlight.rb
+++ Formula/fullsimlight.rb
@@ -3,6 +3,9 @@ class Fullsimlight < Formula
   desc "FullSimLight"
   homepage "https://gitlab.cern.ch/GeoModelDev/FullSimLight"
 
+  url "https://qat.pitt.edu/FullSimLight-1.0.0.tar.gz"
+  sha256 "675de51a555cebaef14f56940f212d7c6c31a399b7d3d71c4a58a5ea54deb391"
+  
   head do
     url "https://gitlab.cern.ch/GeoModelDev/FullSimLight.git"
   end
