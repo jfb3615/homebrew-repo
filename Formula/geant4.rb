a0370a51eb3f7475a2696ef54e4bd8e06b7f42a6
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2021-04-06 18:24:38 -0400

    Update geant4.rb

--- Formula/geant4.rb
+++ Formula/geant4.rb
@@ -8,9 +8,7 @@ class Geant4 < Formula
   #depends_on "xerces-c"
    def install
      mkdir "builddir" do
-     system "which cmake"
-     system "cmake --version" 
-     system "cmake",  "-DGEANT4_USE_SYSTEM_EXPAT=OFF -DGEANT4_USE_GDML=ON -DGEANT4_BUILD_MULTITHREADED=ON", *std_cmake_args, ".."
+     system "/usr/local/bin/cmake",  "-DGEANT4_USE_SYSTEM_EXPAT=OFF -DGEANT4_USE_GDML=ON -DGEANT4_BUILD_MULTITHREADED=ON", *std_cmake_args, ".."
      system "make",      "install"
     
     end
