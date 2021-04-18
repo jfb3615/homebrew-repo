3d11e5366184f7a447c5d7f37d96844ccd6c81d2
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2021-04-18 11:20:30 -0400

    Update geomodeltools.rb

--- Formula/geomodeltools.rb
+++ Formula/geomodeltools.rb
@@ -14,6 +14,7 @@ class Geomodeltools < Formula
   
   bottle do
     root_url "https://qat.pitt.edu/Bottles"
+    sha256 cellar: :any, big_sur: "fe78d7821c684b1bfbc9052a2edcff17d36abf4c2b9afe293d61357e0c12de95"
     sha256 catalina: "c68bf843e16d566b2c1590d29ce33f4b0801d77d8eae83d465166bbc7940c01d"
   end
 
