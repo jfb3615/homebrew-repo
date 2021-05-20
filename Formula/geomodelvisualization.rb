d6c1e8348c78c97427ea351ddb94af7c1f6eb7d3
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2021-05-20 19:05:41 -0400

    Update geomodelvisualization.rb

--- Formula/geomodelvisualization.rb
+++ Formula/geomodelvisualization.rb
@@ -30,7 +30,7 @@ class Geomodelvisualization < Formula
   
  bottle do
     root_url "https://qat.pitt.edu/Bottles"
-    sha256 big_sur: "af0209675a385830efb9dcf358acf969ef3b8dad35c56b2e119637ede3afc24f"
+    sha256 big_sur: "1abca2c685da27d98e040bd17b22514509cf7fc48132250e425ff5fdf2605d2d"
     sha256 cellar: :any, catalina: "3c6018b4cbfdd536c60ad949ee8687c1f30dfd66a6af571538e758fa91da2b25"
  end
 
