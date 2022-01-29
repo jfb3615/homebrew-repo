ba8700fe209f83495efb5591080e9a90b3e0e3e6
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2022-01-29 07:24:17 -0500

    Update geomodelg4.rb

--- Formula/geomodelg4.rb
+++ Formula/geomodelg4.rb
@@ -4,8 +4,8 @@ class Geomodelg4 < Formula
   homepage "https://gitlab.cern.ch/GeoModelDev/GeoModelG4"
    
   
-  url "https://qat.pitt.edu/GeoModel-4.2.1.tar.gz"
-  sha256 "2e000a90de06264227ef902abea5aa305dc2050a270f6bd073bdf6542a060dec"
+  url "https://qat.pitt.edu/GeoModel-4.2.4.tar.gz"
+  sha256 "4a7a1f5a237daabed83d0b8f9f4586e6671d9abddd47b6f4e59ca578f9086287"
   
   head do
     url "https://gitlab.cern.ch/GeoModelDev/GeoModel.git"
