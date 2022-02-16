1787cd1cf464d5f45548a5c40a611e5ad51e636d
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2022-02-16 16:47:53 -0500

    Update geomodeltools.rb

--- Formula/geomodeltools.rb
+++ Formula/geomodeltools.rb
@@ -3,8 +3,8 @@ class Geomodeltools < Formula
   desc "GeoModelTools"
   homepage "https://gitlab.cern.ch/GeoModelDev"
    
-  url "https://qat.pitt.edu/GeoModel-4.2.4.tar.gz"
-  sha256 "4a7a1f5a237daabed83d0b8f9f4586e6671d9abddd47b6f4e59ca578f9086287"
+  url "https://qat.pitt.edu/GeoModel-4.2.6.tar.gz"
+  sha256 "fffcfec5340f0317cb686d17d5cabcce3c2875e3ac88031eb03db50c95bcac47"
   
   head do
     url "https://gitlab.cern.ch/GeoModelDev/GeoModel.git"
