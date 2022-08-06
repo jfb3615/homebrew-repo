c6427f04e7d3382267bdcfc7b7db285d5980c8b1
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2022-08-06 11:00:42 -0400

    Update spacetime.rb

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -21,7 +21,6 @@ class Spacetime < Formula
   
   bottle do
     root_url "https://qat.pitt.edu/Bottles"
-    cellar :any
     sha256 "6048a8f2117254755dc8d2d5b07c6742470a7c836a534303c88836c3db3d57af" => :catalina
   end
 
