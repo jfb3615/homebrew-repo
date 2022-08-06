b2ec463d948e7bfdf9c0a617c63fe835f4c263a7
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2022-08-06 10:59:41 -0400

    Update spacetime.rb

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -4,7 +4,7 @@
 class Spacetime < Formula
   desc "The Spacetime Class Library is a small collection of classes intended to facilitate numerical computations in nonrelativistic and relativistic quantum mechanic"
   homepage "qat.pitt.edu"
-  url "https://www.qat.pitt.edu/spacetime-3.0.0.tar.gz"
+  url "https://www.qat.pitt.edu/spacetime-3.0.1.tar.gz"
   sha256 "0d2ac17ac2877a3c983310721f7c43b1e6fd7d06a51b5776af0b8a5127fdf0bc"
   depends_on 'eigen' 
   depends_on 'pkg-config' 
