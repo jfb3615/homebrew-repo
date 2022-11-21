980800312e706492a08a4bfd9e7afe71081d0c79
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2022-11-21 18:10:56 -0500

    Update geant4.rb

--- Formula/geant4.rb
+++ Formula/geant4.rb
@@ -1,7 +1,7 @@
 class Geant4 < Formula
   desc "Geant4 simulation development toolkit"
   homepage "https://geant4.web.cern.ch"
-  url "https://qat.pitt.edu/geant4-v10.6.1.tar.gz"
+  url "https://qat.pitt.edu/geant4-v11.0.3.tar.gz"
   sha256 "4fd64149ae26952672a81ce5579d3806fda4bd251d486897093ac57633a42b7e"
   depends_on "cmake" => :build
   depends_on "doxygen" => "build"
