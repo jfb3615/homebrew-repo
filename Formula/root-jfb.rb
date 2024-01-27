a625f53f7aa480d2abae0491c3d54df05fe91860
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2024-01-27 09:29:00 +0100

    Update root-jfb.rb

--- Formula/root-jfb.rb
+++ Formula/root-jfb.rb
@@ -62,17 +62,7 @@ class RootJfb < Formula
   end
 
   def install
-    ENV.append "LDFLAGS", "-Wl,-rpath,#{lib}/root"
-    ENV.remove "HOMEBREW_INCLUDE_PATHS", Formula["util-linux"].opt_include if OS.mac?
-
-    inreplace "cmake/modules/SearchInstalledSoftware.cmake" do |s|
-      # Enforce secure downloads of vendored dependencies. These are
-      # checksummed in the cmake file with sha256.
-      s.gsub! "http://lcgpackages", "https://lcgpackages"
-      # Patch out check that skips using brewed glew.
-      s.gsub! "CMAKE_VERSION VERSION_GREATER 3.15", "CMAKE_VERSION VERSION_GREATER 99.99"
-    end
-
+   
     args = std_cmake_args + %W[
       -DCMAKE_CXX_STANDARD=17
       -Dbuiltin_glew=ON
@@ -93,68 +83,19 @@ class RootJfb < Formula
 
     # Homebrew now sets CMAKE_INSTALL_LIBDIR to /lib, which is incorrect
     # for ROOT with gnuinstall, so we set it back here.
-    system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args(install_libdir: "lib/root")
+    #system "cmake", "-S", ".", "-B", "builddir", *args, *std_cmake_args(install_libdir: "lib/root")
     system "cmake", "--build", "builddir"
     system "ctest", "-R", "tutorial-tree", "--verbose", "--parallel", ENV.make_jobs, "--test-dir", "builddir"
     system "cmake", "--install", "builddir"
 
     chmod 0755, bin.glob("*.*sh")
 
-    pth_contents = "import site; site.addsitedir('#{lib}/root')\n"
-    (prefix/Language::Python.site_packages(python3)/"homebrew-root.pth").write pth_contents
+    #pth_contents = "import site; site.addsitedir('#{lib}/root')\n"
+    #(prefix/Language::Python.site_packages(python3)/"homebrew-root.pth").write pth_contents
   end
 
-  def caveats
-    <<~EOS
-      As of ROOT 6.22, you should not need the thisroot scripts; but if you
-      depend on the custom variables set by them, you can still run them:
+ 
 
-      For bash users:
-        . #{HOMEBREW_PREFIX}/bin/thisroot.sh
-      For zsh users:
-        pushd #{HOMEBREW_PREFIX} >/dev/null; . bin/thisroot.sh; popd >/dev/null
-      For csh/tcsh users:
-        source #{HOMEBREW_PREFIX}/bin/thisroot.csh
-      For fish users:
-        . #{HOMEBREW_PREFIX}/bin/thisroot.fish
-    EOS
-  end
 
-  test do
-    (testpath/"test.C").write <<~EOS
-      #include <iostream>
-      void test() {
-        std::cout << "Hello, world!" << std::endl;
-      }
-    EOS
-
-    # Test ROOT command line mode
-    system bin/"root", "-b", "-l", "-q", "-e", "gSystem->LoadAllLibraries(); 0"
-
-    # Test ROOT executable
-    assert_equal "\nProcessing test.C...\nHello, world!\n",
-                 shell_output("#{bin}/root -l -b -n -q test.C")
-
-    # Test ACLiC
-    assert_equal "\nProcessing test.C+...\nHello, world!\n",
-                 shell_output("#{bin}/root -l -b -n -q test.C+")
-
-    # Test linking
-    (testpath/"test.cpp").write <<~EOS
-      #include <iostream>
-      #include <TString.h>
-      int main() {
-        std::cout << TString("Hello, world!") << std::endl;
-        return 0;
-      }
-    EOS
-    flags = %w[cflags libs ldflags].map { |f| "$(#{bin}/root-config --#{f})" }
-    flags << "-Wl,-rpath,#{lib}/root"
-    shell_output("$(#{bin}/root-config --cxx) test.cpp #{flags.join(" ")}")
-    assert_equal "Hello, world!\n", shell_output("./a.out")
-
-    # Test Python module
-    system python3, "-c", "import ROOT; ROOT.gSystem.LoadAllLibraries()"
-  end
 end
 
