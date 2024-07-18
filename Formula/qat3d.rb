94ef6b8865e81b06415241e2f60f42b2d581a91a
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2024-07-18 13:40:30 -0700

    Update qat3d.rb

--- Formula/qat3d.rb
+++ Formula/qat3d.rb
@@ -1,9 +1,9 @@
 class Qat3d < Formula
   desc "The QAT Package for computation in the physical sciences"
   homepage "qat.pitt.edu"
-  url "https://www.qat.pitt.edu/qat-3.0.3.tar.gz"
+  url "https://www.qat.pitt.edu/qat-5.0.0.tar.gz"
   sha256 "f6d99788acf526c3fe85d4e3a2e77d63b50dfb0e8668aa8e27d231a576bac90a"
-  depends_on 'qt5'
+  depends_on 'qt6'
   depends_on 'eigen' 
   depends_on 'gsl' 
   depends_on 'hdf5' 
