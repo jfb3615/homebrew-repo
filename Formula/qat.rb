e9f532e9490b53312bf4799318b87c28611cb4c4
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2025-06-26 12:15:29 +0200

    Update qat.rb
    Take the distribution directly from GitHub. Update to version 6.0.0. This uses qt6.

--- Formula/qat.rb
+++ Formula/qat.rb
@@ -1,9 +1,9 @@
 class Qat < Formula
   desc "The QAT Package for computation in the physical sciences"
   homepage "qat.pitt.edu"
-  url "https://www.qat.pitt.edu/qat-3.0.3.tar.gz"
-  sha256 "f6d99788acf526c3fe85d4e3a2e77d63b50dfb0e8668aa8e27d231a576bac90a"
-  depends_on 'qt5' 
+  url "https://github.com/jfb3615/qat/archive/refs/tags/6.0.0.tar.gz 
+  sha256 "e62b9d52659f785e4d9755a55844b2bf8431e1967dfbffb13efc3a42122f6d9f"
+  depends_on 'qt' 
   depends_on 'eigen' 
   depends_on 'gsl' 
   depends_on 'hdf5' 
@@ -20,10 +20,10 @@ class Qat < Formula
     system "make", "install"
   end
  
-  bottle do
-    root_url "https://qat.pitt.edu/Bottles"
-    sha256 arm64_monterey: "17375fa42029522fb64e482abd2aa41e15cf38c0ad549d2fb041a44402a7878e"
-  end
+#  bottle do
+#    root_url "https://qat.pitt.edu/Bottles"
+#    sha256 arm64_monterey: "17375fa42029522fb64e482abd2aa41e15cf38c0ad549d2fb041a44402a7878e"
+#  end
 
   
 #  def test 
