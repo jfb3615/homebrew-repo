0109d57d873e317473f422d216b50d775cf007de
jfb3615 <42502420+jfb3615@users.noreply.github.com>
2025-06-26 13:55:49 +0200

    Update qat.rb

--- Formula/qat.rb
+++ Formula/qat.rb
@@ -17,7 +17,7 @@ class Qat < Formula
     mkdir "build" do
       system "pwd"
       system "cat",   "../CMakeLists.txt"
-      system "cmake", "..", *std_cmake_args
+      system "cmake ..", *std_cmake_args
       system "make"
       system "make", "install"
     end    
