6b0cd94d1b3789f40873f62a57869a407cf48cc7
Joseph Boudreau <42502420+jfb3615@users.noreply.github.com>
2026-08-29 18:44:16 -0400

    Update sha256 and remove duplicate dependency

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -5,9 +5,8 @@ class Spacetime < Formula
   desc "The Spacetime Class Library is a small collection of classes intended to facilitate numerical computations in nonrelativistic and relativistic quantum mechanic"
   homepage "qat.pitt.edu"
   url "https://gitlab.cern.ch/boudreau/spacetime/-/archive/3.0.3/spacetime-3.0.3.tar.gz"
-  sha256 "e2fcb319e4a85610d66f83773211de658b6bcb6a443ee0539368a132bbf8422c"
-  depends_on 'eigen' 
-  depends_on 'pkg-config' 
+  sha256 "375f119c2fd49633ca1a5145442f721ed3bf66c80256bc5abd5080b3283fbad6"
+  depends_on 'eigen'
   depends_on "cmake" => :build
    
   def install
