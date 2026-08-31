1e7dc16a8cbb24ec7e89277f06b7714cbd8d5340
Joseph Boudreau <42502420+jfb3615@users.noreply.github.com>
2026-08-31 09:54:13 -0400

    Update spacetime URL to version 3.0.4

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -4,7 +4,7 @@
 class Spacetime < Formula
   desc "The Spacetime Class Library is a small collection of classes intended to facilitate numerical computations in nonrelativistic and relativistic quantum mechanic"
   homepage "qat.pitt.edu"
-  url "https://gitlab.cern.ch/boudreau/spacetime/-/archive/3.0.3/spacetime-3.0.3.tar.gz"
+  url "https://gitlab.cern.ch/boudreau/spacetime/-/archive/3.0.3/spacetime-3.0.4.tar.gz"
   sha256 "d0f2a28daf494567127fb0b8eac41f77f5e2dff3558ab65ddbf61036ae096d7b"
   depends_on 'eigen'
   depends_on "cmake" => :build
