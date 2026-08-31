cc954e97f2cd17f73879505dee3de083bff6097a
Joseph Boudreau <42502420+jfb3615@users.noreply.github.com>
2026-08-31 09:55:26 -0400

    Update sha256 checksum for spacetime.rb

--- Formula/spacetime.rb
+++ Formula/spacetime.rb
@@ -5,7 +5,7 @@ class Spacetime < Formula
   desc "The Spacetime Class Library is a small collection of classes intended to facilitate numerical computations in nonrelativistic and relativistic quantum mechanic"
   homepage "qat.pitt.edu"
   url "https://gitlab.cern.ch/boudreau/spacetime/-/archive/3.0.3/spacetime-3.0.4.tar.gz"
-  sha256 "d0f2a28daf494567127fb0b8eac41f77f5e2dff3558ab65ddbf61036ae096d7b"
+  sha256 "bbea0f088a7f8fe4ba7cf637fefdeec5228b2a24c622f37c62396d8c0af47727"
   depends_on 'eigen'
   depends_on "cmake" => :build
    
