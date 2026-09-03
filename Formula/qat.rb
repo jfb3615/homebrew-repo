918d8ededc0061505be2bd88c87a0120bf14562a
Joseph Boudreau <42502420+jfb3615@users.noreply.github.com>
2026-09-03 08:19:52 -0400

    Update sha256 checksum for QAT package

--- Formula/qat.rb
+++ Formula/qat.rb
@@ -2,7 +2,7 @@ class Qat < Formula
   desc "The QAT Package for computation in the physical sciences"
   homepage "qat.pitt.edu"
   url "https://github.com/jfb3615/qat/archive/refs/tags/6.1.3.tar.gz"
-  sha256 "9f4af8e798c9646807824330faa33c7559a51ea09281198c4277144f62a8edc2"
+  sha256 "b0ee7e78dedd909a74a2dd176ce1c454acb4bdcfacf860e0a940c0776101950e"
   depends_on 'qt' 
   depends_on 'eigen' 
   depends_on 'gsl' 
